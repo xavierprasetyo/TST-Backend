b1c73c969bcdb8b14a112a72b4f117a3291588b3
xavierprasetyo <xavierprasetyo89@gmail.com>
2020-12-12 12:50:15 +0700

    adding logger

--- logger.go
+++ logger.go
@@ -1,13 +1,48 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"strings"
+
+	"cloud.google.com/go/firestore"
 )
 
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		log.Println(req.URL.Path)
+		var api string
+		path := strings.ReplaceAll(req.URL.Path, "/api", "")
+		switch path {
+		case "/items":
+			api = "Get_Items"
+			break
+		case "/items/add":
+			api = "Add_Item"
+			break
+		case "/items/check":
+			api = "Check_Item"
+			break
+		case "/items/cross":
+			api = "Cross_Item"
+			break
+		case "/items/delete":
+			api = "Delete_Item"
+			break
+		}
+		client, ctx, err := createClient()
+		if err != nil {
+			sendError(w, http.StatusInternalServerError, "Error Creating Firebase Client")
+			return
+		}
+		_, err = client.Collection("log").Doc(api).Update(ctx, []firestore.Update{
+			{
+				Path:  "count",
+				Value: firestore.Increment(1),
+			},
+		})
+		if err != nil {
+			sendError(w, http.StatusInternalServerError, "Error in Logger")
+			return
+		}
 		next.ServeHTTP(w, req)
 	})
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,18 +20,6 @@ func main() {
 	googleOauthId = os.Getenv("GOOGLE_OAUTH_ID")
 
 	fmt.Println("Start")
-	// items = append(items, Item{
-	// 	ID:      "1",
-	// 	Name:    "Telur",
-	// 	Checked: false,
-	// 	Crossed: true,
-	// })
-	// items = append(items, Item{
-	// 	ID:      "2",
-	// 	Name:    "Ayam",
-	// 	Checked: true,
-	// 	Crossed: true,
-	// })
 
 	router := mux.NewRouter()
 	apiRouter := router.PathPrefix("/api").Subrouter()
@@ -47,13 +34,6 @@ func main() {
 
 }
 
-func Middleware(h http.Handler, middleware ...func(http.Handler) http.Handler) http.Handler {
-	for _, mw := range middleware {
-		h = mw(h)
-	}
-	return h
-}
-
 func HandlerInit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -61,19 +41,4 @@ func HandlerInit(next http.Handler) http.Handler {
 		w.Header().Set("Content-Type", "application/json")
 		next.ServeHTTP(w, req)
 	})
-}
-
-func test(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var t Auth
-	err := decoder.Decode(&t)
-	if err != nil {
-		panic(err)
-	}
-	isValid, error := verifyIdToken(t.Token)
-	if error == nil {
-		log.Println(isValid)
-	} else {
-		panic(error)
-	}
 }
