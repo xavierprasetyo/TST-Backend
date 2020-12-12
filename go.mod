85974c2040ec355f297cf5eb8ab79789e8a8354f
xavierprasetyo <xavierprasetyo89@gmail.com>
2020-12-12 15:42:37 +0700

    adding cors

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	cloud.google.com/go/firestore v1.4.0
 	firebase.google.com/go v3.13.0+incompatible
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
 	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
@@ -27,7 +28,7 @@ func main() {
 	router := mux.NewRouter()
 	apiRouter := router.PathPrefix("/api").Subrouter()
 	apiRouter.Use(HeaderInit, AuthMiddleware, Logger)
-	apiRouter.HandleFunc("/items", getItems).Methods(http.MethodGet)
+	apiRouter.HandleFunc("/items", getItems).Methods(http.MethodGet, http.MethodOptions)
 	apiRouter.HandleFunc("/items/add", addItem).Methods(http.MethodPost)
 	apiRouter.HandleFunc("/items/check", checkItem).Methods(http.MethodPut)
 	apiRouter.HandleFunc("/items/cross", crossItem).Methods(http.MethodPut)
@@ -37,5 +38,5 @@ func main() {
 	apiRouter.HandleFunc("/log/reset", resetLogHandler).Methods(http.MethodPost)
 	apiRouter.HandleFunc("/log", getLog).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "DELETE"}), handlers.AllowedOrigins([]string{"*"}))(router)))
 }
